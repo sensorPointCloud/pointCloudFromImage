04ecf793522a5fff86e9f3a9a239f6ac6cd8a7bb
kbk <sensorPointCloud@protonmail.com>
2020-11-08 22:08:00 +0100

    Cleaning

--- sensor/plot_pointclouds.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pptk
-
-#val  = np.load('results/initial dot laser/dist_3_single_path_threshold_50_erosion_dilution_False_210_blk_dot.npy')
-val  = np.load('dist_3_single_path_threshold_50_erosion_dilution_False_210_blk_dot_mid_floor.npy')
-v = pptk.viewer(val, val[:, 2])
-#v.attributes(val[['r', 'g', 'b']] / 255., val['i'])
-v.set(point_size=5)
-v.wait()
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# import cam_estimate_laser_angle
-# import distance_estimation
-# import distance_from_files
